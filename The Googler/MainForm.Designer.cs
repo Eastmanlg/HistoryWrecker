8b7a840dddef93497c8ff8482e5863c61ea71749
Logan Eastman <eastmanlg@gmail.com>
2019-07-29 14:50:43 -0600

    Added checkbox to not actually google these things

---
+++ The Googler/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace The_Googler
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoogle = new System.Windows.Forms.Button();
+            this.cbxActuallySearch = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnGoogle
+            // 
+            this.btnGoogle.Location = new System.Drawing.Point(12, 12);
+            this.btnGoogle.Name = "btnGoogle";
+            this.btnGoogle.Size = new System.Drawing.Size(178, 26);
+            this.btnGoogle.TabIndex = 0;
+            this.btnGoogle.Text = "Press Here";
+            this.btnGoogle.UseVisualStyleBackColor = true;
+            this.btnGoogle.Click += new System.EventHandler(this.btnGoogle_Click);
+            // 
+            // cbxActuallySearch
+            // 
+            this.cbxActuallySearch.AutoSize = true;
+            this.cbxActuallySearch.Location = new System.Drawing.Point(12, 44);
+            this.cbxActuallySearch.Name = "cbxActuallySearch";
+            this.cbxActuallySearch.Size = new System.Drawing.Size(98, 17);
+            this.cbxActuallySearch.TabIndex = 1;
+            this.cbxActuallySearch.Text = "Actually google";
+            this.cbxActuallySearch.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 67);
+            this.Controls.Add(this.cbxActuallySearch);
+            this.Controls.Add(this.btnGoogle);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "The Googler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoogle;
+        private System.Windows.Forms.CheckBox cbxActuallySearch;
+    }
+}
+
---
+++ The Googler/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Googler
+{
+    public partial class Main : Form
+    {
+        
+        string[] questions = { "Can you", "How will I", "Is it normal to", "Can I", "Is it legal to", "How to", "Would my mom hate me if I", "Should I confess that I", "Why does my son", "How to stop dog from" };
+        string[] verbs = { "marry", "kiss", "inject", "fuck", "hang", "swallow", "stroke", "fill", "talk to", "view", "see", "experience", "slam", "emancipate" };
+        string[] nouns = { "my cousin", "cows", "farm animals", "iguanas", "French people", "frogs", "fictional characters", "my 'friend'", "my sister", "pigs", "my priest", "my boss" };
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoogle_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random(DateTime.Now.Millisecond);
+
+            string question = questions[rnd.Next(0, questions.Length)];
+            string verb = verbs[rnd.Next(0, verbs.Length)];
+            string noun = nouns[rnd.Next(0, nouns.Length)];
+
+            string query = String.Format("{0} {1} {2}?", question, verb, noun);
+
+            if (cbxActuallySearch.Checked)
+            {
+                System.Diagnostics.Process.Start(GetFirstResult(query).ToString());
+            }
+            else
+            {
+                MessageBox.Show(query);
+            }
+            
+        }
+        
+        public Uri GetFirstResult(string term)
+        {
+            // Use the I'm Feeling Lucky URL
+            var url = string.Format("https://www.google.com/search?num=100&site=&source=hp&q={0}&btnI=1", term);
+
+            HttpWebRequest req = HttpWebRequest.CreateHttp(url);
+            return req.GetResponse().ResponseUri;
+        }
+    }
+}
